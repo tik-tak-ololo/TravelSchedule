291c5e40a44976f15ba1fe858ca1307bc564aad8
tik-tak-ololo <tik-tak-ololo@outlook.com>
2026-07-03 01:36:25 +0700

    Initial commit

---
+++ TravelSchedule/TravelScheduleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelScheduleApp.swift
+//  TravelSchedule
+//
+//  Created by Сергей Хмелёв on 03.07.2026.
+//
+
+import SwiftUI
+
+@main
+struct TravelScheduleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
